2e96e09a9cd182741186b397ec42b6df6bfb24d5
MiAir <xkof3st.mail@gmail.com>
2024-08-12 21:06:42 +0300

    Рефакторинг Удалены неверные файлы

--- core/src/main/java/me/kofesst/lovemood/core/usecases/profile/ProfileUseCases.kt
+++
@@ -1,9 +0,0 @@
-package me.kofesst.lovemood.core.usecases.profile
-
-import me.kofesst.lovemood.core.models.Profile
-import me.kofesst.lovemood.core.usecases.crud.CrudUseCases
-
-/**
- * CRUD use cases модели профиля.
- */
-interface ProfileUseCases : CrudUseCases<Profile>
--- core/src/main/java/me/kofesst/lovemood/core/usecases/profile/RelationshipUseCases.kt
+++
@@ -1,9 +0,0 @@
-package me.kofesst.lovemood.core.usecases.profile
-
-import me.kofesst.lovemood.core.models.Relationship
-import me.kofesst.lovemood.core.usecases.crud.CrudUseCases
-
-/**
- * CRUD use cases модели отношений.
- */
-interface RelationshipUseCases : CrudUseCases<Relationship>
