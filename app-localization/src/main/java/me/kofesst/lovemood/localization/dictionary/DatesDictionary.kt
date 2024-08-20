763d6e2c52e351c4adc74311c24d0ef320371567
Dmitry Aleksandrov <xkof3st.mail@gmail.com>
2024-08-20 14:18:34 +0300

    Форматирование длительности вынесено в app-localization

--- app-localization/src/main/java/me/kofesst/lovemood/localization/dictionary/DatesDictionary.kt
+++ app-localization/src/main/java/me/kofesst/lovemood/localization/dictionary/DatesDictionary.kt
@@ -1,7 +1,14 @@
 package me.kofesst.lovemood.localization.dictionary
 
 import android.content.Context
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
+import me.kofesst.lovemood.core.text.AppTextHolder
 import me.kofesst.lovemood.core.text.TextCasesHolder
+import me.kofesst.lovemood.features.date.DatePeriod
 import me.kofesst.lovemood.localization.R
 import me.kofesst.lovemood.localization.ResourceText
 
@@ -50,4 +57,41 @@ class DatesDictionary(appContext: Context) {
         secondCase = yearsSecondCase,
         thirdCase = yearsThirdCase
     )
+
+    fun buildAnnotatedPeriodString(
+        period: DatePeriod,
+        ifPeriodEmpty: String = "",
+        unitSpanStyle: SpanStyle = SpanStyle(fontWeight = FontWeight.Bold),
+        unitNameSpanStyle: SpanStyle = SpanStyle(),
+        unitsDelimiter: String = " "
+    ): AnnotatedString = buildAnnotatedString {
+        val units = mapOf(
+            yearsTextCases to period.years,
+            monthsTextCases to period.months,
+            daysTextCases to period.days
+        ).filter { it.value > 0 }
+        if (units.isEmpty()) {
+            append(ifPeriodEmpty)
+        }
+        units.keys.forEachIndexed { index, unitNameHolder ->
+            val unit = units[unitNameHolder] ?: -1
+            appendPeriodUnit(unit, unitNameHolder, unitSpanStyle, unitNameSpanStyle)
+            if (index != units.size - 1) append(unitsDelimiter)
+        }
+    }
+
+    private fun AnnotatedString.Builder.appendPeriodUnit(
+        unit: Int,
+        unitNameHolder: AppTextHolder,
+        unitSpanStyle: SpanStyle,
+        unitNameSpanStyle: SpanStyle
+    ) {
+        withStyle(unitSpanStyle) {
+            append(unit.toString())
+        }
+        append(" ")
+        withStyle(unitNameSpanStyle) {
+            append(unitNameHolder.string(unit, false))
+        }
+    }
 }
--- app/src/main/java/me/kofesst/lovemood/presentation/screens/home/sections/LoveDurationSection.kt
+++ app/src/main/java/me/kofesst/lovemood/presentation/screens/home/sections/LoveDurationSection.kt
@@ -10,20 +10,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.AnnotatedString
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.withStyle
 import kotlinx.coroutines.delay
 import me.kofesst.lovemood.R
 import me.kofesst.lovemood.app.LocalDictionary
 import me.kofesst.lovemood.app.dictionary
 import me.kofesst.lovemood.core.models.Relationship
-import me.kofesst.lovemood.core.text.AppTextHolder
 import me.kofesst.lovemood.core.ui.components.cards.BaseCard
 import me.kofesst.lovemood.features.date.DatePeriod
-import me.kofesst.lovemood.ui.text.dictionary.AppDictionary
 import java.time.LocalDate
 import kotlin.time.Duration.Companion.minutes
 
@@ -56,51 +49,13 @@ fun LoveDurationSection(
             backgroundImagePainter = painterResource(R.drawable.ic_love_clock),
             label = dictionary.screens.home.loveDurationSectionLabel.string()
         ) {
+            val dictionary = LocalDictionary.current
             Text(
-                text = buildAnnotatedString {
-                    appendPeriod(
-                        period = lovePeriod,
-                        dictionary = LocalDictionary.current
-                    )
-                }
+                text = dictionary.dates.buildAnnotatedPeriodString(
+                    period = lovePeriod,
+                    ifPeriodEmpty = dictionary.screens.home.startDateWhenUnitsAreEmpty.string()
+                )
             )
         }
-    }
-}
-
-private fun AnnotatedString.Builder.appendPeriod(
-    period: DatePeriod,
-    dictionary: AppDictionary,
-    unitSpanStyle: SpanStyle = SpanStyle(fontWeight = FontWeight.Bold),
-    unitNameSpanStyle: SpanStyle = SpanStyle(),
-    unitsDelimiter: String = " "
-) {
-    val units = mapOf(
-        dictionary.dates.yearsTextCases to period.years,
-        dictionary.dates.monthsTextCases to period.months,
-        dictionary.dates.daysTextCases to period.days
-    ).filter { it.value > 0 }
-    if (units.isEmpty()) {
-        append(dictionary.screens.home.startDateWhenUnitsAreEmpty.string())
-    }
-    units.keys.forEachIndexed { index, unitNameHolder ->
-        val unit = units[unitNameHolder] ?: -1
-        appendPeriodUnit(unit, unitNameHolder, unitSpanStyle, unitNameSpanStyle)
-        if (index != units.size - 1) append(unitsDelimiter)
-    }
-}
-
-private fun AnnotatedString.Builder.appendPeriodUnit(
-    unit: Int,
-    unitNameHolder: AppTextHolder,
-    unitSpanStyle: SpanStyle = SpanStyle(fontWeight = FontWeight.Bold),
-    unitNameSpanStyle: SpanStyle = SpanStyle()
-) {
-    withStyle(unitSpanStyle) {
-        append(unit.toString())
-    }
-    append(" ")
-    withStyle(unitNameSpanStyle) {
-        append(unitNameHolder.string(unit, false))
     }
 }
