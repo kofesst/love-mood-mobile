be8946bdbbe6967a968d69fb4f7608fa9a144a51
Дмитрий <xkof3st.mail@gmail.com>
2024-09-16 19:26:18 +0300

    Обновление целевой версии Android SDK до 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,12 +9,12 @@ plugins {
 
 android {
     namespace = "me.kofesst.lovemood"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "me.kofesst.lovemood"
         minSdk = 28
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 2
         versionName = "0.0.2"
 
